2300af6fa16a46fe45940bc7aae9ffc3589e9f4a
Pero <g00422221@atu.ie>
2022-12-18 02:14:45 +0000

    added ability to click on preview images to select image on tomato sidte

--- tomato.js
+++ tomato.js
@@ -45,3 +45,31 @@ function prevImage()
     document.getElementById("img"+(counter+1)).style.borderWidth="5px";
 }
 
+function selectImg1()
+{
+    document.getElementById("img1").style.borderWidth="0px";
+    document.getElementById("img2").style.borderWidth="0px";
+    document.getElementById("img3").style.borderWidth="0px";
+
+    document.getElementById("tomato").innerHTML=imagestr[0];
+    document.getElementById("img1").style.borderWidth="5px";}
+
+function selectImg2()
+{
+    document.getElementById("img1").style.borderWidth="0px";
+    document.getElementById("img2").style.borderWidth="0px";
+    document.getElementById("img3").style.borderWidth="0px";
+
+    document.getElementById("tomato").innerHTML=imagestr[1];
+    document.getElementById("img2").style.borderWidth="5px";}
+
+
+function selectImg3()
+{
+    document.getElementById("img1").style.borderWidth="0px";
+    document.getElementById("img2").style.borderWidth="0px";
+    document.getElementById("img3").style.borderWidth="0px";
+
+    document.getElementById("tomato").innerHTML=imagestr[2];
+    document.getElementById("img3").style.borderWidth="5px";}
+
